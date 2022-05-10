fc652009b71abc34ed88f46a0856ce4cba316d2e
CesarPalomino <60803729+CesarPalomino@users.noreply.github.com>
2022-05-10 16:35:31 -0500

    Update client.py

--- client.py
+++ client.py
@@ -7,6 +7,18 @@
 #
 #
 from socket import socket
+
+import nacl.utils
+
+from nacl.public import PrivateKey, SealedBox
+import getpass
+import pickle
+
+skfile = PrivateKey.generate()
+pkfile = skfile.public_key
+
+sealed_box = SealedBox(pkfile)
+
 def main():
     s = socket()
     s.connect(("localhost", 6030))
@@ -17,20 +29,20 @@ def main():
         
         while content:
             # Enviar contenido.
-            s.send(content)
+            s.sendall(content)
             content = f.read(1024)
-        
+            encrypted = sealed_box.encrypt(f.encode())
         break
     
     # Se utiliza el caracter de código 1 para indicar
     # al cliente que ya se ha enviado todo el contenido.
     try:
-        s.send(chr(1))
+        s.sendall(chr(1))
     except TypeError:
         # Compatibilidad con Python 3.
         s.send(bytes(chr(1), "utf-8"))
     
-    # Cerrar conexión y archivo.
+    # Cerrar conexión y encriptar archivo.
     s.close()
     f.close()
     print("El archivo ha sido enviado correctamente.")
